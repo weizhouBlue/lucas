e9924c66666fbf000a0f3f158d76b21fb8a24dfb
weizhoulan <weizhou.lan@daocloud.io>
2019-06-18 21:03:38 +0800

    new image

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,8 @@
 
 
 etcd_addr="https://10.6.185.110:12379"
+#etcd_addr="https://10.6.185.110:12379,https://10.6.185.120:12379"
+
 WEBUI_PORT="8088"
 
 
@@ -10,13 +12,14 @@ hostpath_etcd_cert=`find /etc -name "etcd-cert"`
 hostpath_etcd_key=`find /etc -name "etcd-key"`
 
 #------------------------------------------
-
+image="weizhoulan/etcdv3_browser:latest"
 path_etcd_ca=`basename $hostpath_etcd_ca`
 path_etcd_cert=`basename $hostpath_etcd_cert`
 path_etcd_key=`basename $hostpath_etcd_key`
 
 
-docker run -d -p $WEBUI_PORT:8080   \
+docker pull $image
+docker run -d -p $WEBUI_PORT:8080  --name etcdv3-browser \
   -e CA_FILE=/var/$path_etcd_ca \
   -e CERT_FILE=/var/$path_etcd_cert \
   -e KEY_FILE=/var/$path_etcd_key \
@@ -24,4 +27,4 @@ docker run -d -p $WEBUI_PORT:8080   \
     -v $hostpath_etcd_cert:/var/$path_etcd_cert   \
     -v $hostpath_etcd_key:/var/$path_etcd_key     \
   -e ENDPOINTS="$etcd_addr" \
-  registry.cn-hangzhou.aliyuncs.com/ringtail/lucas:0.0.1
+  $image
